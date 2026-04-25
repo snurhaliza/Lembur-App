61e2c140d3eabc5ae2f492e3526124ed2877ce9c
snurhaliza <snurhalizah372@gmail.com>
2026-04-26 00:44:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-const GAS_URL="https://script.google.com/macros/s/AKfycbybks5dvFwX8LbdFCfVL9Jb5UI3841AAaTOjEE6l3YkA80IWGkHqx-kVFLicfRLFfhQ/exec";
+const GAS_URL="https://script.google.com/macros/s/AKfycbyJnanMe4OREBwdUjFyQ0Tiiv7emMkwJZNGYJB2IqFRbLfrM6tMrWNhyGDdAexbXgau/exec";
 
 async function login(){
 
-  const passInput = document.getElementById("password").value.trim();
-  const roleInput = document.getElementById("role").value;
+  let pass = document.getElementById("password").value;
+  let role = document.getElementById("role").value;
 
-  if(!passInput){
+  if(!pass){
     msg.innerText="Password wajib diisi!";
     return;
   }
 
-  try{
+  let res = await fetch(`${GAS_URL}?action=login&password=${pass}`);
+  let data = await res.json();
 
-    const res = await fetch(`${GAS_URL}?action=login&password=${passInput}`);
-    const data = await res.json();
-
-    if(!data.status){
-      msg.innerText="NIK tidak ditemukan!";
-      return;
-    }
-
-    if(data.role !== roleInput){
-      msg.innerText="Role tidak sesuai!";
-      return;
-    }
-
-    localStorage.setItem("user", JSON.stringify(data));
-
-    location.href = data.role==="admin"
-      ? "admin.html"
-      : "karyawan.html";
-
-  }catch(e){
-    msg.innerText="Server error!";
+  if(!data.status){
+    msg.innerText="Login gagal!";
+    return;
   }
+
+  if(data.role !== role){
+    msg.innerText="Role tidak sesuai!";
+    return;
+  }
+
+  localStorage.setItem("user", JSON.stringify(data));
+
+  location.href = role==="admin" ? "admin.html" : "karyawan.html";
 }
