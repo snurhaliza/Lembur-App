47785e181bbdd8af34a76337b4c9ce29559643c3
snurhaliza <snurhalizah372@gmail.com>
2026-04-26 00:53:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const GAS_URL="https://script.google.com/macros/s/AKfycbyJnanMe4OREBwdUjFyQ0Tiiv7emMkwJZNGYJB2IqFRbLfrM6tMrWNhyGDdAexbXgau/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbz977NPlRuMLfpD4PMFsEBPOTO_sS_t7iCdcdZMNZVBhNqLkVtMc10dsV-Y1gqm8l33/exec";
 
 async function login(){
 
-  let pass = document.getElementById("password").value;
+  let pass = document.getElementById("password").value.trim();
   let role = document.getElementById("role").value;
 
   if(!pass){
-    msg.innerText="Password wajib diisi!";
+    msg.innerText = "Password wajib!";
     return;
   }
 
   let res = await fetch(`${GAS_URL}?action=login&password=${pass}`);
-  let data = await res.json();
+  let d = await res.json();
 
-  if(!data.status){
-    msg.innerText="Login gagal!";
+  if(!d.status){
+    msg.innerText = "Login gagal!";
     return;
   }
 
-  if(data.role !== role){
-    msg.innerText="Role tidak sesuai!";
+  if(d.role !== role){
+    msg.innerText = "Role tidak sesuai!";
     return;
   }
 
-  localStorage.setItem("user", JSON.stringify(data));
+  localStorage.setItem("user", JSON.stringify(d));
 
   location.href = role==="admin" ? "admin.html" : "karyawan.html";
 }
