1cf947161a7802b5682997a11ad2f215fa490670
snurhaliza <snurhalizah372@gmail.com>
2026-04-25 21:52:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwDHaRgVaNDr3Oe2zhMgZZ52W-R-UdlQ_poFELzMHlZhFITzJt6EAYGrueExjC2f5k/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxh4yXDGDs_PcnQbZ4IEhtsYIRSXMvluDLiLXvjtCV_U8GjkMHxs2lSS_eivvnToj9M/exec";
 
 async function login(){
+
   let password = document.getElementById("password").value;
   let role = document.getElementById("role").value;
+
+  if(!password){
+    msg.innerText = "Password wajib diisi!";
+    return;
+  }
 
   let res = await fetch(`${GAS_URL}?action=login&password=${password}`);
   let data = await res.json();
 
   if(!data.status){
-    msg.innerText = "Login gagal";
+    msg.innerText = "Login gagal!";
     return;
   }
 
   if(data.role !== role){
-    msg.innerText = "Role salah!";
+    msg.innerText = "Role tidak sesuai!";
     return;
   }
 
   localStorage.setItem("user", JSON.stringify(data));
 
-  if(role==="admin") location.href="admin.html";
-  else location.href="karyawan.html";
+  location.href = role==="admin" ? "admin.html" : "karyawan.html";
 }
