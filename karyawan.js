6a3516399e47bde98f8b0ac2802891f6b88007e3
snurhaliza <snurhalizah372@gmail.com>
2026-04-25 21:27:35 +0700

    Update karyawan.js

--- karyawan.js
+++ karyawan.js
@@ -1,30 +1,23 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwtWmjiLibJ320aegedisIa-ouw5H-buLZ8yU085MLNHfgUheNGDr-f8hE-sRqkFZWu/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwDHaRgVaNDr3Oe2zhMgZZ52W-R-UdlQ_poFELzMHlZhFITzJt6EAYGrueExjC2f5k/exec";
 
 let user = JSON.parse(localStorage.getItem("user"));
 
-document.getElementById("nik").value = user.nik;
-document.getElementById("nama").value = user.nama;
-document.getElementById("tanggal").value = new Date().toISOString().split("T")[0];
+nik.value = user.nik;
+nama.value = user.nama;
+tanggal.value = new Date().toISOString().split("T")[0];
 
 function menu(id){
   ["dash","input","saya"].forEach(i=>{
     document.getElementById(i).style.display="none";
   });
   document.getElementById(id).style.display="block";
-
-  if(id==="saya"){
-    document.getElementById("infoUser").innerText =
-    `Nama: ${user.nama}\nNIK: ${user.nik}`;
-  }
 }
 
 mulai.oninput = akhir.oninput = function(){
-  let a = new Date("2000-01-01 "+mulai.value);
-  let b = new Date("2000-01-01 "+akhir.value);
-
+  let a = new Date("2000 "+mulai.value);
+  let b = new Date("2000 "+akhir.value);
   let jam = (b-a)/3600000;
   if(jam<0) jam+=24;
-
   total.value = jam.toFixed(1);
 };
 
@@ -39,14 +32,10 @@ async function simpan(){
       mulai:mulai.value,
       akhir:akhir.value,
       total:total.value,
-      keterangan:keterangan.value
+      keterangan:keterangan.value,
+      tipe:jenis.value
     })
   });
 
   alert("Tersimpan");
-}
-
-function logout(){
-  localStorage.clear();
-  window.location.href="index.html";
 }
