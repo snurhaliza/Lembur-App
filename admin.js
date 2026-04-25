ec6440ecf1230a45e04ece9e475c7c1255385de3
snurhaliza <snurhalizah372@gmail.com>
2026-04-25 11:40:01 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,3 +1,19 @@
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwE3gjqJJZyX4wTZhds943XidSS7Vyf7WkwgxJ5Drm0gl9tCf4URqfK7dZQ4f7wEPsK/exec";
+
+function loadDashboard(){
+  fetch(GAS_URL+"?action=dashboard")
+  .then(r=>r.json())
+  .then(d=>{
+    today.innerHTML = d.today;
+    month.innerHTML = d.month;
+    user.innerHTML = d.chart.length;
+  });
+}
+
+function logout(){
+  localStorage.clear();
+  window.location.href="index.html";
+}
 function loadData(){
   fetch(GAS_URL+"?action=data")
   .then(r=>r.json())
