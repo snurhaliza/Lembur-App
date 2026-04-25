91c82ff576ee420d7e4d416cc822309892d5fce3
snurhaliza <snurhalizah372@gmail.com>
2026-04-25 12:42:39 +0700

    Update karyawan.js

--- karyawan.js
+++ karyawan.js
@@ -1,22 +1,23 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwE3gjqJJZyX4wTZhds943XidSS7Vyf7WkwgxJ5Drm0gl9tCf4URqfK7dZQ4f7wEPsK/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzZNWLPacwWkh3bLn3MDjsAFuasWJM450PY-H9ariYe3sgVYKWjguqQen0feDlaUfoP/exec";
 
 let user = JSON.parse(localStorage.getItem("user"));
 
-// AUTO FILL
 document.getElementById("nik").value = user.nik;
 document.getElementById("nama").value = user.nama;
 document.getElementById("tanggal").value = new Date().toISOString().split("T")[0];
 
-// NAV
-function show(id){
-  dashboard.style.display="none";
-  input.style.display="none";
-  grafik.style.display="none";
-
+function menu(id){
+  ["dash","input","saya"].forEach(i=>{
+    document.getElementById(i).style.display="none";
+  });
   document.getElementById(id).style.display="block";
+
+  if(id==="saya"){
+    document.getElementById("infoUser").innerText =
+    `Nama: ${user.nama}\nNIK: ${user.nik}`;
+  }
 }
 
-// HITUNG JAM
 mulai.oninput = akhir.oninput = function(){
   let a = new Date("2000-01-01 "+mulai.value);
   let b = new Date("2000-01-01 "+akhir.value);
@@ -25,11 +26,10 @@ mulai.oninput = akhir.oninput = function(){
   if(jam<0) jam+=24;
 
   total.value = jam.toFixed(1);
-}
+};
 
-// SIMPAN
-function simpan(){
-  fetch(https://script.google.com/macros/s/AKfycbwE3gjqJJZyX4wTZhds943XidSS7Vyf7WkwgxJ5Drm0gl9tCf4URqfK7dZQ4f7wEPsK/execL,{
+async function simpan(){
+  await fetch(GAS_URL,{
     method:"POST",
     body:JSON.stringify({
       action:"simpan",
@@ -41,21 +41,11 @@ function simpan(){
       total:total.value,
       keterangan:keterangan.value
     })
-  })
-  .then(r=>r.json())
-  .then(()=>alert("Tersimpan"));
-}
-function cekStatus(){
-  fetch(GAS_URL+"?action=dashboard")
-  .then(r=>r.json())
-  .then(d=>{
-    status.innerText = d.todayCount > 0 
-      ? "SUDAH INPUT" 
-      : "BELUM INPUT";
   });
+
+  alert("Tersimpan");
 }
 
-// LOGOUT
 function logout(){
   localStorage.clear();
   window.location.href="index.html";
