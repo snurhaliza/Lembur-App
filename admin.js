5e0e490d7b6088b4d22f4cf31ee9a79ae7f30e2c
snurhaliza <snurhalizah372@gmail.com>
2026-04-25 11:36:59 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,16 +1,24 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycby3dxo47AHIEjrrpS6tLBIJaUrLtV_J_xZRsp5M23rgzlFO9GKeRcIIcHy7o38mBSJl/exec";
-
-function loadDashboard(){
-  fetch(GAS_URL+"?action=dashboard")
+function loadData(){
+  fetch(GAS_URL+"?action=data")
   .then(r=>r.json())
-  .then(d=>{
-    today.innerHTML = d.today;
-    month.innerHTML = d.month;
-    user.innerHTML = d.chart.length;
-  });
-}
+  .then(data=>{
 
-function logout(){
-  localStorage.clear();
-  window.location.href="index.html";
+    let html = "";
+
+    data.forEach(d=>{
+      html += `
+      <tr>
+        <td>${d.tanggal}</td>
+        <td>${d.nik}</td>
+        <td>${d.nama}</td>
+        <td>${d.total}</td>
+        <td>${d.keterangan}</td>
+        <td>
+          <button onclick="hapus(${d.id})">Hapus</button>
+        </td>
+      </tr>`;
+    });
+
+    table.innerHTML = html;
+  });
 }
